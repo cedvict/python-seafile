68993a1955f54dec154a8c7b0eb1c5507215240f
Cedric FOWOUE <c.fowoue@lugarsarl.com>
2022-06-07 09:55:26 +0200

    fusion of fork leaf

--- seafileapi_extended/__init__.py
+++ seafileapi_extended/__init__.py
@@ -1,12 +1,12 @@
 """Seafile api client"""
-from seafileapi.client import Groups
 from seafileapi.exceptions import ClientHttpError, OperationError, DoesNotExist
-from seafileapi.group import Group
 from seafileapi.repo import RepoRevision
 
 from seafileapi_extended.admin import SeafileAdmin
 from seafileapi_extended.client import SeafileApiClient
 from seafileapi_extended.exceptions import UserExisted, GroupExisted
+from seafileapi_extended.group import Group, AdminGroup, GroupMember
+from seafileapi_extended.groups import Groups, AdminGroups
 from seafileapi_extended.files import SeafDir, SeafFile
 from seafileapi_extended.repo import Repo
 from seafileapi_extended.repos import Repos
@@ -17,8 +17,11 @@ __version__ = "1.0.0"
 
 __all__ = [
     SeafileAdmin.__name__,
+    AdminGroups.__name__,
     Groups.__name__,
+    AdminGroup.__name__,
     Group.__name__,
+    GroupMember.__name__,
     ClientHttpError.__name__,
     OperationError.__name__,
     DoesNotExist.__name__,
